e22bc90d06f84198ec05e9f48a7c9a60e7c9e931
Konstantinos Gkousaris <kostasgkousaris@gmail.com>
2021-03-11 18:50:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ int main() {
 	Room r5(104, 1, "Suite", "All-include, Sea-view, Mini bar , Private Security", 3, false);
 	Room r6(105, 1, "Suite", "All-include, Sea-view, Mini bar , Private Security", 3, false);
 
-	f.insertA(new CheckIn(1000, "Nikos", "Spirou", 14, r1, "Bed and Breakfast"));
-	f.insertA(new Transfer(1001, "Hara", "Verra", 14, r1, r4, "Air condition is out of order"));
+	f.insertA(new CheckIn(1000, "Name_1", "Last_Name_1", 14, r1, "Bed and Breakfast"));
+	f.insertA(new Transfer(1001, "Name_2", "Last_Name_2", 14, r1, r4, "Air condition is out of order"));
 	f.FIFOQueueDisplay(myfile);
 	
 	//TODO the display of a list and write in text file
